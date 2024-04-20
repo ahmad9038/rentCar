c496b3d82fe8c84db329c35efb7d64f71d8dcd29
ahmadShahzad <128154734+ahmad9038@users.noreply.github.com>
2024-04-20 09:27:42 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,13 +13,13 @@ const viewsPath = path.join(process.cwd(), "views");
 app.use(express.static(viewsPath));
 app.use(cors());
 app.use(express.json());
-// app.use(
-//   cors({
-//     origin: ["https://deploy-mer-lwhq.vercel.app"],
-//     methods: ["POST", "GET"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: ["https://rent-a-car-wine.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 dbConnection();
 
