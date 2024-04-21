0f42f89a4c84062aaa9d10101c4896796303823c
ahmadShahzad <128154734+ahmad9038@users.noreply.github.com>
2024-04-22 00:12:35 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,8 +9,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const viewsPath = path.join(process.cwd(), "views");
-app.use(express.static(viewsPath));
+// const viewsPath = path.join(process.cwd(), "views");
+// app.use(express.static(viewsPath));
+app.use(express.static(path.join(__dirname, 'views')));
 app.use(cors());
 app.use(express.json());
 
