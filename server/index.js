6467397fe42223a75558b6c5c91a8cbf8ccf76f2
ahmadShahzad <128154734+ahmad9038@users.noreply.github.com>
2024-04-22 00:19:09 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,11 +4,15 @@ import dbConnection from "./dbConnection.js";
 import dotenv from "dotenv";
 import router from "./routes/index.js";
 import path from "path";
-import { dirname } from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // const viewsPath = path.join(process.cwd(), "views");
 // app.use(express.static(viewsPath));
