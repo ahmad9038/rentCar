47d5c521a5bf6e47d6d50def9d387fb86d4b11f3
ahmadShahzad <128154734+ahmad9038@users.noreply.github.com>
2024-04-22 00:04:53 +0500

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -10,23 +10,13 @@ import {
 import path from "path";
 const router = express.Router();
 const __dirname = path.resolve(path.dirname(""));
-import ejs from 'ejs';
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/verify/:userId/:token", verifyEmail);
 
 router.get("/verified", (req, res) => {
-  const filePath = path.join(__dirname, "./server/../views/verifiedpage.html");
-  // Render the HTML using ejs
-  ejs.renderFile(filePath, {}, (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Error rendering verification page');
-    } else {
-      res.send(data);
-    }
-  });
+  res.sendFile(path.join(__dirname, "./server/../views/verifiedpage.html"));
 });
 
 //password reset
