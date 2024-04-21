3603941f71787a43fa2b363cd91a9040b949f672
ahmadShahzad <128154734+ahmad9038@users.noreply.github.com>
2024-04-22 00:20:38 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,22 +4,22 @@ import dbConnection from "./dbConnection.js";
 import dotenv from "dotenv";
 import router from "./routes/index.js";
 import path from "path";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// const viewsPath = path.join(process.cwd(), "views");
-// app.use(express.static(viewsPath));
-app.use(express.static(path.join(__dirname, 'views')));
+const viewsPath = path.join(process.cwd(), "views");
+app.use(express.static(viewsPath));
 app.use(cors());
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: ["https://rent-a-car-frontend-fawn.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 dbConnection();
 
